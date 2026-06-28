c35d25b65e256088c0de967becb6edcbec14e8b5
siklands-glitch <siklands@gmail.com>
2026-06-28 13:34:28 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Automated Bot",
         // Activity type number (0 = Playing).
         type: 0,
       },
